4844405f30ab31f7045240c887e50276700f7cc6
Hamza Nayyer <hamzanayyer@gmail.com>
2020-05-07 05:45:33 +0200

    add new component in header file

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,23 @@ import React, { Component } from 'react'
 export default class Header extends Component {
 
   render () {
-    return <header>Header</header>
+    return (
+        <header className="navbar navbar-expand-lg navbar-lighth bg-light">
+          <a className="navbar-brand" href="/">AllMovie</a>
+          <button className="navbar-toggler">
+            <span className="navbar-toggler-icon"></span>
+          </button>  
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Home</a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Favoris</a>
+              </li>
+            </ul>
+          </div>
+        </header>
+    )
   }
 }
