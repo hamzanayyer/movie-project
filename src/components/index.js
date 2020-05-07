52d2b5b9b85e9e29afd0521c161ee83db65345a1
Hamza Nayyer <hamzanayyer@gmail.com>
2020-05-07 04:45:07 +0200

    add new project

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from './header/Header';
+export { default as MovieDetails } from './movie-details/MovieDetails';
+export { default as MovieList} from './movie-list/MovieList'
