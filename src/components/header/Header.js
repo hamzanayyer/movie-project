f253cf9fbc3754e8146779a159b276440a005fe7
Hamza Nayyer <hamzanayyer@gmail.com>
2020-05-07 06:00:16 +0200

    update Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,17 +4,17 @@ export default class Header extends Component {
 
   render () {
     return (
-        <header className="navbar navbar-expand-lg navbar-lighth bg-light">
-          <a className="navbar-brand" href="/">AllMovie</a>
-          <button className="navbar-toggler">
-            <span className="navbar-toggler-icon"></span>
-          </button>  
+      <header className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">AlloMovie</a>
+      <button class="navbar-toggler">
+        <span class="navbar-toggler-icon"></span>
+      </button>
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
                 <a className="nav-link" href="/">Home</a>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <a className="nav-link" href="/">Favoris</a>
               </li>
             </ul>
