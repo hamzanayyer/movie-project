a41655214817089d63a7483e9fc6636fb45d0f71
Hamza Nayyer <hamzanayyer@gmail.com>
2020-05-15 08:58:10 +0200

    add sass and update movieList

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
-import MovieElement from './MovieElement';
+import React, { Component } from 'react';
+import MovieElement from './MovieElement/MovieElement';
 
 export default class MovieList extends Component {
-
-  render () {
+  render() {
     return (
-      <div className="w-75 d-flex flex-wrap">
-        <MovieElement movie={this.props.movies[0]} updateSelectedMovie={this.props.updateSelectedMovie} />
-        <MovieElement movie={this.props.movies[1]} updateSelectedMovie={this.props.updateSelectedMovie} />
-        <MovieElement movie={this.props.movies[2]} updateSelectedMovie={this.props.updateSelectedMovie} />
-        <MovieElement movie={this.props.movies[3]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+      <div className="w-75 d-flex flex-row flex-wrap align-content-start">
+        { this.props.movies.map( (m, index) => (
+          <MovieElement key={ m.title + index } movie={ m } updateSelectedMovie={ () => { this.props.updateSelectedMovie(index) } } />
+        ))}
       </div>
-    )
+    );
   }
+
 }
